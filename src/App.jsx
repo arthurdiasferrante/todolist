fb70ed119baa92689b68469eb4a4ea105a175eda
Arthur <arthurdiasferrante@gmail.com>
2025-02-14 14:14:35 -0300

    inital layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="bg-blue-500 text-white text-center p-4">
-        Tailwind está funcionando! 🚀
-      </div>
-    </>
+    <div className="app">
+      <h1 className="text-slate-800 text-center p-4">
+        To-Do List
+      </h1>
+
+      <form className=''>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa..."
+        />
+        <button type="submit" className='bg-blue-800 text-center p-3'>Adicionar</button>
+      </form>
+        
+        <ul className='list-none p-2 m-0 rounded-lg'>
+          <li>passea com meg</li>
+          <li>passea com digao</li>
+          <li>oejkgwg</li>
+          <li>rheahk</li>
+        </ul>
+
+    </div>
   )
 }
 
